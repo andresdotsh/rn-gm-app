d32d27b8140178a86bfe76854732f0f8193914df
Andres Rosero Velasquez <andresmedia84@gmail.com>
2024-09-30 17:24:46 -0500

    feat: icons added

--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Ionicons name='glasses-sharp' size={30} color='black' />
       <Text
         style={{ fontSize: 20, fontFamily: 'Ubuntu400' }}
       >{`Hello World!`}</Text>
