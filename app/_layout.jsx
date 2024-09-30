dfb84f529a423880e798aaab838d57421eca7f41
Andres Rosero Velasquez <andresmedia84@gmail.com>
2024-09-30 17:14:27 -0500

    feat: ubuntu font added

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,47 @@
 import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { useEffect } from 'react'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  return <Stack />
+  const [fontsLoaded, fontsLoadError] = useFonts({
+    Ubuntu400: require('../assets/fonts/UbuntuSansMono/Regular.ttf'),
+    Ubuntu400Italic: require('../assets/fonts/UbuntuSansMono/RegularItalic.ttf'),
+    Ubuntu500: require('../assets/fonts/UbuntuSansMono/Medium.ttf'),
+    Ubuntu500Italic: require('../assets/fonts/UbuntuSansMono/MediumItalic.ttf'),
+    Ubuntu600: require('../assets/fonts/UbuntuSansMono/SemiBold.ttf'),
+    Ubuntu600Italic: require('../assets/fonts/UbuntuSansMono/SemiBoldItalic.ttf'),
+    Ubuntu700: require('../assets/fonts/UbuntuSansMono/Bold.ttf'),
+    Ubuntu700Italic: require('../assets/fonts/UbuntuSansMono/BoldItalic.ttf'),
+  })
+
+  useEffect(() => {
+    if (fontsLoaded || fontsLoadError) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded, fontsLoadError])
+
+  if (!fontsLoaded && !fontsLoadError) {
+    return null
+  }
+
+  return (
+    <>
+      <StatusBar style='auto' />
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: 'lightgray',
+          },
+          headerTitleStyle: {
+            color: '#000',
+          },
+          headerTitle: '',
+        }}
+      />
+    </>
+  )
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{`Hello World!`}</Text>
-      <StatusBar style='auto' />
+      <Text
+        style={{ fontSize: 20, fontFamily: 'Ubuntu400' }}
+      >{`Hello World!`}</Text>
     </View>
   )
 }
