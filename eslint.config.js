ab3bd14756f3455034b6abf2b9b2066991f86ae8
Andres Rosero Velasquez <andresmedia84@gmail.com>
2024-11-20 10:18:16 -0500

    fix: app.json

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,6 @@ import EslintConfigPrettier from 'eslint-config-prettier'
 import EslintPluginReact from 'eslint-plugin-react'
 import EslintPluginReactNative from 'eslint-plugin-react-native'
 import EslintPluginPrettier from 'eslint-plugin-prettier'
-import { FlatCompat } from '@eslint/eslintrc'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
 
 export default [
   EslintConfigPrettier,
